3a94c4767b19cdc602f79dc9d33a5027bd440689
parteekcoder <parteekgoyal118@gmail.com>
2023-04-17 11:19:52 +0530

    initializing the app

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+import pages.*;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Home().setVisible(true);
+            }
+        });
+    }
+}
